287d6b0ef23cb075bd7f562b44d1f49684bf396c
Saksham Saxena <sakshamsaxena9410@gmail.com>
2025-09-21 23:23:19 +0530

    Day 20q1 .c

--- Day 20q1 .c
+++ Day 20q1 .c
@@ -1,1 +1,36 @@
-d
+#include <stdio.h>
+
+int main() {
+    int num, digit;
+    unsigned long long product = 1;
+    int oddFound = 0; 
+
+    // Input number
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // Handle negative numbers by taking absolute value
+    if (num < 0) {
+        num = -num;
+    }
+
+    // Loop to extract digits and multiply odd ones
+    while (num > 0) {
+        digit = num % 10;
+        if (digit % 2 != 0) {  // Check if odd digit
+            product *= digit;
+            oddFound = 1;
+        }
+        num = num / 10;
+    }
+
+   
+    if (oddFound) {
+        printf("Product of odd digits = %llu\n", product);
+    } else {
+        printf("No odd digits found in the number.\n");
+    }
+
+    return 0;
+}
+
