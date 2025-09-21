85581d8ce70b5f43c6b1fad1006f001bd28209ea
Saksham Saxena <sakshamsaxena9410@gmail.com>
2025-09-21 23:21:53 +0530

    day 19 Q2 .c

--- day 19 Q2 .c
+++ day 19 Q2 .c
@@ -1,1 +1,21 @@
-r
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0, digit;
+
+    // Input from user
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // Loop to extract and add digits
+    while (num > 0) {
+        digit = num % 10;     
+        sum += digit;         
+        num = num / 10;       
+    }
+
+  
+    printf("Sum of digits = %d\n", sum);
+
+    return 0;
+}
