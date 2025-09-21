901854cbf9290a38f0ab4fe3c480b98fe22d58a4
Saksham Saxena <sakshamsaxena9410@gmail.com>
2025-09-21 23:17:49 +0530

    Day 18 Q1.c

--- Day 18 Q1.c
+++ Day 18 Q1.c
@@ -1,1 +1,22 @@
-r
+#include <stdio.h>
+
+int main() {
+    int num, i;
+
+    // Input from user
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    printf("Factors of %d are: ", num);
+
+    // Loop to find and print factors
+    for (i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
