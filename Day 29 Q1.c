fd773fc79f75fbd8269d81c3132cda08c8876896
Saksham Saxena <sakshamsaxena9410@gmail.com>
2025-09-21 23:46:32 +0530

    Day 29 Q1.c

---
+++ Day 29 Q1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];  // Declare array of size n
+
+    // Read elements
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Calculate sum
+    for(i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    // Print sum
+    printf("Sum of array elements = %d\n", sum);
+
+    return 0;
+}
