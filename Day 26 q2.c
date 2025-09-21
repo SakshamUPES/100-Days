7c294c7ab3b225816856ba3abbb68733e5576cc5
Saksham Saxena <sakshamsaxena9410@gmail.com>
2025-09-21 23:38:31 +0530

    Day 26 q2.c

---
+++ Day 26 q2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    // Group 1: 1 star
+    for (i = 1; i <= 1; i++) {
+        printf("*\n");
+    }
+    printf("\n");  // Blank line
+
+    // Group 2: 4 stars
+    for (i = 1; i <= 4; i++) {
+        printf("*\n");
+    }
+    printf("\n");  // Blank line
+
+    // Group 3: 5 stars
+    for (i = 1; i <= 5; i++) {
+        printf("*\n");
+    }
+    printf("\n");  // Blank line
+
+    // Group 4: 3 stars
+    for (i = 1; i <= 3; i++) {
+        printf("*\n");
+    }
+    printf("\n");  // Blank line
+
+    // Group 5: 1 star
+    for (i = 1; i <= 1; i++) {
+        printf("*\n");
+    }
+
+    return 0;
+}
