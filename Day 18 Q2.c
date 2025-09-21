06bfb617f62aa0294e1abab33c01bb0fca5bf3d0
Saksham Saxena <sakshamsaxena9410@gmail.com>
2025-09-21 23:19:07 +0530

    Day 18 Q2.c

--- Day 18 Q2.c
+++ Day 18 Q2.c
@@ -1,1 +1,23 @@
-y
+#include <stdio.h>
+
+int main() {
+    int a, b, i, hcf;
+
+   
+    printf("Enter two positive integers: ");
+    scanf("%d %d", &a, &b);
+
+    // Find the smaller number
+    int min = (a < b) ? a : b;
+
+
+    for (i = 1; i <= min; i++) {
+        if (a % i == 0 && b % i == 0) {
+            hcf = i;  // Update HCF
+        }
+    }
+
+    printf("HCF (GCD) of %d and %d is: %d\n", a, b, hcf);
+
+    return 0;
+}
