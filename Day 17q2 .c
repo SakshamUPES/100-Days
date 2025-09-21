a62967601643a8d2fa1b3bb62d04c582925e9354
Saksham Saxena <sakshamsaxena9410@gmail.com>
2025-09-21 23:16:51 +0530

    Day 17q2 .c

--- Day 17q2 .c
+++ Day 17q2 .c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int num, i, isPrime = 1;  
+
+    // Input from user
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    // Handle special cases
+    if (num <= 1) {
+        printf("%d is not a prime number.\n", num);
+        return 0;
+    }
+
+
+    for (i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            isPrime = 0;  // Found a divisor
+            break;
+        }
+    }
+
+    // Output result
+    if (isPrime) {
+        printf("%d is a prime number.\n", num);
+    } else {
+        printf("%d is not a prime number.\n", num);
+    }
+
+    return 0;
+}
 
