d19f8cb88695e9d675e86d77554bf93379236995
Saksham Saxena <sakshamsaxena9410@gmail.com>
2025-10-22 00:00:54 +0530

    Day 42 q2.c

--- Day 42 q2.c
+++ Day 42 q2.c
@@ -1,1 +1,19 @@
-g
+#include <stdio.h>
+
+int main() {
+    char str[100];
+
+    printf("Enter a lowercase string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Loop through each character and convert to uppercase if lowercase
+    for(int i = 0; str[i] != '\0' && str[i] != '\n'; i++) {
+        if(str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - ('a' - 'A');
+        }
+    }
+
+    printf("Converted string in uppercase: %s", str);
+
+    return 0;
+}
