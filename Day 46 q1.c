15f2dbbf89331d709f6edfb145782e7a1098021c
Saksham Saxena <sakshamsaxena9410@gmail.com>
2025-10-22 23:47:45 +0530

    Day 46 q1.c

---
+++ Day 46 q1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char str[100], result[100];
+    int i, j = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        // Check if the character is a vowel
+        if (!(str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U' ||
+              str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')) {
+            result[j++] = str[i];
+        }
+    }
+    result[j] = '\0'; // Null-terminate the result string
+
+    printf("String after removing vowels: %s\n", result);
+
+    return 0;
+}
