b381591090ea14a5ba58c3e3b0ac09d8e93c0285
Saksham Saxena <sakshamsaxena9410@gmail.com>
2025-11-19 00:04:54 +0530

    Day 72.c

--- Day 72.c
+++ Day 72.c
@@ -1,1 +1,24 @@
-d
+#include <stdio.h>
+
+int main() {
+    FILE *file;
+    char line[256];
+
+    // Open the file in read mode
+    file = fopen("info.txt", "r");
+    if (file == NULL) {
+        printf("Could not open info.txt\n");
+        return 1;
+    }
+
+    // Read and print each line until EOF
+    while (fgets(line, sizeof(line), file)) {
+        printf("%s", line);
+    }
+
+    // Close the file
+    fclose(file);
+
+    return 0;
+}
+
