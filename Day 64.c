82b4b92bbbb7373b41a247c6689f04922c1d09c8
Saksham Saxena <sakshamsaxena9410@gmail.com>
2025-11-18 23:57:28 +0530

    Day 64.c

--- Day 64.c
+++ Day 64.c
@@ -1,1 +1,33 @@
-e
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main() {
+    char s[1000];
+    printf("Enter the string: ");
+    scanf("%s", s);
+
+    int n = strlen(s);
+    int max_len = 0;
+
+    for (int i = 0; i < n; i++) {
+        bool visited[256] = {false};  // Track characters in the current substring
+        for (int j = i; j < n; j++) {
+            if (visited[(unsigned char)s[j]]) {
+                // Character already seen, stop expanding this substring
+                break;
+            } else {
+                visited[(unsigned char)s[j]] = true;
+                int curr_len = j - i + 1;
+                if (curr_len > max_len) {
+                    max_len = curr_len;
+                }
+            }
+        }
+    }
+
+    printf("Length of the longest substring without repeating characters is: %d\n", max_len);
+
+    return 0;
+}
+
