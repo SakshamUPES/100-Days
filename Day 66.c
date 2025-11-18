33cac6fc2b5dcc0a907f94e6c2220923cc8c1eee
Saksham Saxena <sakshamsaxena9410@gmail.com>
2025-11-18 23:59:00 +0530

    Day 66.c

--- Day 66.c
+++ Day 66.c
@@ -1,1 +1,31 @@
-d
+
+#include <stdio.h>
+
+int main() {
+    int n, target;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int nums[n];
+    printf("Enter %d positive integers:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    printf("Enter the target value: ");
+    scanf("%d", &target);
+
+    // Brute force nested loop to find the pair
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (nums[i] + nums[j] == target) {
+                printf("%d %d\n", i, j);
+                return 0;
+            }
+        }
+    }
+
+    // If no solution found
+    printf("-1 -1\n");
+    return 0;
+}
