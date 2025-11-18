24fc4ff7b36541e086dd67d6e0ebfb194410baf4
Saksham Saxena <sakshamsaxena9410@gmail.com>
2025-11-19 00:02:27 +0530

    Day 70.c

--- Day 70.c
+++ Day 70.c
@@ -1,1 +1,26 @@
-d
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[1000];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Convert entire string to lowercase first
+    for (int i = 0; str[i] != '\0'; i++) {
+        str[i] = tolower((unsigned char)str[i]);
+    }
+
+    // Capitalize first alphabetic character
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (isalpha((unsigned char)str[i])) {
+            str[i] = toupper((unsigned char)str[i]);
+            break;
+        }
+    }
+
+    printf("Sentence case: %s", str);
+    return 0;
+}
+
