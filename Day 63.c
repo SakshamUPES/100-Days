08bdac26b504c247b7e8a75eff79a8b9a790dd16
Saksham Saxena <sakshamsaxena9410@gmail.com>
2025-11-18 23:56:12 +0530

    Day 63.c

--- Day 63.c
+++ Day 63.c
@@ -1,1 +1,41 @@
-r
+#include <stdio.h>
+
+int main() {
+    int n, k;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the value of k: ");
+    scanf("%d", &k);
+
+    if (k <= 0 || k > n) {
+        printf("Invalid value of k\n");
+        return 1;
+    }
+
+    // Simple selection sort to sort the array
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        // Swap arr[i] and arr[min_idx]
+        int temp = arr[i];
+        arr[i] = arr[min_idx];
+        arr[min_idx] = temp;
+    }
+
+    printf("The %dth smallest element is: %d\n", k, arr[k - 1]);
+
+    return 0;
+}
+
