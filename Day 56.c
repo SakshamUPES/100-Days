5c6aa57ba02487ea4888f0b202be5878e348c516
Saksham Saxena <sakshamsaxena9410@gmail.com>
2025-11-18 23:49:44 +0530

    Day 56.c

--- Day 56.c
+++ Day 56.c
@@ -1,1 +1,38 @@
-a
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n], next_greater[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Brute force nested loop to find next greater element
+    for (int i = 0; i < n; i++) {
+        next_greater[i] = -1; // Default if no greater found
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] > arr[i]) {
+                next_greater[i] = arr[j];
+                break;
+            }
+        }
+    }
+
+    // Print next greater elements in comma separated fashion
+    for (int i = 0; i < n; i++) {
+        if (i != n - 1) {
+            printf("%d, ", next_greater[i]);
+        } else {
+            printf("%d\n", next_greater[i]);
+        }
+    }
+
+    return 0;
+}
+
