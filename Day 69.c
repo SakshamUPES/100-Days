9e351835a46d900b33f6744f0a43680f0ac6a156
Saksham Saxena <sakshamsaxena9410@gmail.com>
2025-11-19 00:01:33 +0530

    Day 69.c

--- Day 69.c
+++ Day 69.c
@@ -1,1 +1,26 @@
-d
+#include <stdio.h>
+
+int main() {
+    int n;
+    
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    int repeated = 0;
+    
+    // XOR all elements. Since only one element repeats, XOR will give that repeated element
+    for (int i = 0; i < n; i++) {
+        repeated ^= arr[i];
+    }
+    
+    printf("The repeated element is: %d\n", repeated);
+    
+    return 0;
+}
+
