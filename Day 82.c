8db172afe3a39d78d342e3a11ef82b669461e84c
Saksham Saxena <sakshamsaxena9410@gmail.com>
2025-12-01 08:20:57 +0530

    Day 82.c

--- Day 82.c
+++ Day 82.c
@@ -1,1 +1,41 @@
-k
+#include <stdio.h>
+
+enum TrafficLight {
+    RED = 0,
+    YELLOW = 1,
+    GREEN = 2
+};
+
+int main() {
+    enum TrafficLight light;
+    int input;
+    
+    printf("Traffic Light Control\n");
+    printf("0 = RED, 1 = YELLOW, 2 = GREEN\n");
+    printf("Enter traffic light value (0-2): ");
+    
+    scanf("%d", &input);
+    
+    if (input < RED || input > GREEN) {
+        printf("Invalid input! Use 0, 1, or 2 only.\n");
+        return 1;
+    }
+    
+    light = (enum TrafficLight)input;
+    
+    switch (light) {
+        case RED:
+            printf("STOP!\n");
+            break;
+        case YELLOW:
+            printf("WAIT!\n");
+            break;
+        case GREEN:
+            printf("GO!\n");
+            break;
+        default:
+            printf("Unknown signal!\n");
+    }
+    
+    return 0;
+}
